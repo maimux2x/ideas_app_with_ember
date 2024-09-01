825a9a0d9a9874628f5ff33e57865a09ec6989ac
maimux2x <m0smile10often@gmail.com>
2024-09-01 20:30:27 +0900

    Add idea model

---
+++ api/db/migrate/20240901110517_create_ideas.rb
@@ -0,0 +1,10 @@
+class CreateIdeas < ActiveRecord::Migration[7.2]
+  def change
+    create_table :ideas do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
